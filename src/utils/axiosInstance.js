7c20fbc0908cb6ee685663fdf17ffb9f4f984825
Mohamed Amine Rguig <amine.mohamed.rg@gmail.com>
2023-12-26 13:43:44 +0100

    removing api from axios url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL + "/api",
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL,
   headers: { "content-type": "application/json" },
 });
