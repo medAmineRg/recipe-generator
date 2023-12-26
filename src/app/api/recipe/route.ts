6f092c8b959001650653ee47f9301f588c58aac5
Mohamed Amine Rguig <amine.mohamed.rg@gmail.com>
2023-12-26 15:13:38 +0100

    adding duration to the create recipe function to 10

--- src/app/api/recipe/route.ts
+++ src/app/api/recipe/route.ts
@@ -1,4 +1,4 @@
-export const maxDuration = 30; // This function can run for a maximum of 5 seconds
+export const maxDuration = 10; // This function can run for a maximum of 5 seconds
 
 import { NextRequest, NextResponse } from "next/server";
 import OpenAIApi from 'openai';
